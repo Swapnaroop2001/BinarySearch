32464314f2b4dccc6e3a1aeb848dee004d909181
Swapnaroop <swapnaroop2001@gmail.com>
2024-03-29 20:09:06 -0400

    //Binary Search

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,7 +2,19 @@
  * BinarySearch
  */
 public class BinarySearch {
-    public void BinarySearch() {
-        
+
+    //Binary Search
+    public int bSearch(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=(high+low)/2;
+            if(target==nums[mid]) return mid;
+            else if( target>nums[mid]) low=mid+1;
+            else high=mid-1;
+        }
+        return -1;
     }
+
+    //Upper bound & Lower bound Binary search
 }
