b2699a9a986a206bfc7b5f5aa605cc38bea7b551
Swapnaroop <swapnaroop2001@gmail.com>
2024-04-02 14:48:55 -0400

    //69. Sqrt(x)

--- BinarySearch.java
+++ BinarySearch.java
@@ -127,85 +127,86 @@ public class BinarySearch {
     }
 
     // 153. Minimum in Rotated Sorted Array.
-    //Rotation Count.
+    // Rotation Count.
     public int findMin(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
 
         while (low < high) {
             int mid = low + ((high - low) / 2);
-            
-            if (nums[low] < nums[high] || low==high) {
-               return nums[low];
-            } 
 
-            if (nums[mid]<nums[high]) {
-                high=mid;
+            if (nums[low] < nums[high] || low == high) {
+                return nums[low];
             }
-            else{
-                low=mid+1;
+
+            if (nums[mid] < nums[high]) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
         return nums[low];
     }
 
-    //540. Single Element in a Sorted Array
+    // 540. Single Element in a Sorted Array
     public int singleNonDuplicate(int[] nums) {
-        int left=0, right=nums.length-1;
-        while (left<right) {
-            int mid=left+((right-left)/2);
-            if (mid%2==1) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) / 2);
+            if (mid % 2 == 1) {
                 mid--;
             }
 
-            if (nums[mid]!=nums[mid+1]) {
-                right=mid;
-            }
-            else{
-                left=mid+2;
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
             }
         }
         return nums[left];
     }
 
-    //162. Find Peak Element
+    // 162. Find Peak Element
     public int findPeakElement(int[] nums) {
-        int n=nums.length;
-        int low=1;
-        int high=nums.length-1;
+        int n = nums.length;
+        int low = 1;
+        int high = nums.length - 1;
 
-        if(nums[0]>nums[1] || n==1) return 0;
-        if(nums[n-1]>nums[n-2]) return n;
+        if (nums[0] > nums[1] || n == 1)
+            return 0;
+        if (nums[n - 1] > nums[n - 2])
+            return n;
 
-        while (low<high) {
-            int mid= low+((high-low)/2);
-            if (nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]) {
+        while (low < high) {
+            int mid = low + ((high - low) / 2);
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
                 return mid;
             }
 
-            if (nums[mid]>nums[mid-1]) {
-                low=mid+1;
-            }else{
-                high=mid-1;
+            if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return -1;
     }
 
-    //69. Sqrt(x)
+    // 69. Sqrt(x)
     public int mySqrt(int x) {
-        int low=0, high=x;
-        while (low<high) {
-            int mid= low+((high-low)/2);
-            int val=mid*mid;
-            if (val<=x) {
-                low=mid+1;
-            }else{
-                high=mid-1;
+        int low = 0, high = x;
+        while (low < high) {
+            int mid = low + ((high - low) / 2);
+            int val = mid * mid;
+            if (val <= x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
         return high;
     }
+
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6 };
         System.out.println(searchRange(arr, 3));
