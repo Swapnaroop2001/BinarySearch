476dfc7cbabf34abb8e459d8010cd5d380f4d6a0
Swapnaroop <swapnaroop2001@gmail.com>
2024-04-02 14:48:11 -0400

    //162. Find Peak Element

--- BinarySearch.java
+++ BinarySearch.java
@@ -168,6 +168,44 @@ public class BinarySearch {
         return nums[left];
     }
 
+    //162. Find Peak Element
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        int low=1;
+        int high=nums.length-1;
+
+        if(nums[0]>nums[1] || n==1) return 0;
+        if(nums[n-1]>nums[n-2]) return n;
+
+        while (low<high) {
+            int mid= low+((high-low)/2);
+            if (nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]) {
+                return mid;
+            }
+
+            if (nums[mid]>nums[mid-1]) {
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return -1;
+    }
+
+    //69. Sqrt(x)
+    public int mySqrt(int x) {
+        int low=0, high=x;
+        while (low<high) {
+            int mid= low+((high-low)/2);
+            int val=mid*mid;
+            if (val<=x) {
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6 };
         System.out.println(searchRange(arr, 3));
