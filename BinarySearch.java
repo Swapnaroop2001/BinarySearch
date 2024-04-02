ed093db12ad5270b045eb7f7212d00b2b5e767e2
Swapnaroop <swapnaroop2001@gmail.com>
2024-04-02 15:41:18 -0400

    // Find Nth Root Of M

--- BinarySearch.java
+++ BinarySearch.java
@@ -207,6 +207,34 @@ public class BinarySearch {
         return high;
     }
 
+    // Find Nth Root Of M
+    public static int NthRoot(int n, int m) {
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = supportFunction( n,mid, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static int supportFunction(int n, int mid, int m) {
+       long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+
+
+    
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6 };
         System.out.println(searchRange(arr, 3));
