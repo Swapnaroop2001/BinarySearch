a039c5ac868210407547b2253b335f9f29b51088
Swapnaroop <swapnaroop2001@gmail.com>
2024-04-08 12:59:50 -0400

    1539. Kth Missing Positive Number

--- BinarySearch.java
+++ BinarySearch.java
@@ -327,6 +327,21 @@ public class BinarySearch {
         return counter;
     }
 
+    //1539. Kth Missing Positive Number
+    public int findKthPositive(int[] arr, int k) {
+        int low=0, high=arr.length-1;
+        while (low<=high) {
+            int mid=low+((high-low)/2);
+            int missingElem=arr[mid]-(mid+!);
+            if (missingElem<k) {
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return k+high+1;
+    }
+
 
 
 
