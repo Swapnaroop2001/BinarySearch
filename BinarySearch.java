c398b088cab6f3c5ae4f6577cc300238ce33c017
Swapnaroop <swapnaroop2001@gmail.com>
2024-04-10 19:36:12 -0400

    //410. Split Array Largest Sum //Book Allocation

--- BinarySearch.java
+++ BinarySearch.java
@@ -372,9 +372,46 @@ public class BinarySearch {
         return false;
     }
 
+    //410. Split Array Largest Sum
+    //Book Allocation
+    public int splitArray(int[] nums, int k) {
+        int low=0;
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i]>=low) {
+                low=nums[i];
+            }
+            sum=sum+nums[i];
+        }
+        int high=sum;
+        while (low<=high) {
+            int mid=low+((high-low)/2);
+            int students=Count(nums, mid, k);
+            if (students>k) {
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return low;
+    }
+    public int Count(int[] nums,int mid, int k) {
+        int count=1;
+        int sum=0;
+        for (int i = 0; i < nums.length; i++) {
+            if (sum+nums[i]<=mid) {
+                sum+=nums[i];
+            }else{
+                count++;
+                sum=nums[i];
+            }
+        }
+        return count;
+    }
 
 
-
+    
     public static void main(String[] args) {
         int arr[] = { 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6 };
         System.out.println(searchRange(arr, 3));
